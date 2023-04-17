794b4ea9100ed999ed413452461615a5be89cf92
Gabriela Pintado V <125084134+Gabby948@users.noreply.github.com>
2023-04-17 18:16:12 -0500

    4

--- src/data.js
+++ src/data.js
@@ -1,20 +1,43 @@
-// estas funciones son de ejemplo
+//estas funciones son de ejemplo
+import pokemon from "./data/pokemon/pokemon.js";
 
-export const example = () => {
-  return 'example';
+
+export const pokemonData  = () => {
+  return "pokemonData";
 };
-
-export const anotherExample = () => {
-  return 'OMG';
-};
-
-Fetch("url", {
-  method: ("C:\Users\Gabriela Pintado\Desktop\DATA LOVERS\DEV006-data-lovers\src\data\pokemon\pokemon.json ",        //optional
-  headers: {                              //optional
-      Content-Type:" pokemon/json "  //optional
-      ...
-  },
-  body: formData                          //optional
-})
+export const property = pokemon;
 
 
+export default pokemon;
+
+
+// export const pokemonData = () => {
+//   return 'pokemon';
+// };
+// import pokemon from "./data/pokemon/pokemon"
+// import pokemon from "./data/pokemon/pokemon";
+//import propiedades from "./data/pokemon/pokemon.js";
+
+
+//const getData = (one, other) => js.stringify(pokemon.js) === JSON.stringify(pokemon.JSON);
+//console.log()
+
+//let data = []//
+// function pokemonData(propiedades, data) {
+    
+// }
+// const pokemon = ();
+// let result = ;
+
+// function calcArea() {    SOLO EJEMPLO PARA ENTERDER EXPORT
+// 	 return PI * radius * radius;
+// }
+// export {calcArea};
+
+
+
+// const getPokemon=() => {
+//    const pokemonData = pokemon.js ();
+//    createPokemonCard (pokemon);
+// }
+// console.log (pokemonData);
--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
-import { example } from './data.js';
+//mport { pokemonData } from './data.js';
 // import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
+
+import pokemon from "./data/pokemon/pokemon.js";
+ 
+console.log (pokemon)
+
 // import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+// import {num, name, generation, about } from "./pokemon.js";
+
+//window.scrollTo(x-coord, y-coord)  hacer scroll en la las cards.
+
+// window.scrollTo(0,1000);

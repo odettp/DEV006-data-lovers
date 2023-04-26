7ca6d8965e75f802573a837dd13280af2d09bc14
Gabby948 <teffa08-94@hotmail.com>
2023-04-26 17:47:20 -0500

    prueba2

--- src/data.js
+++ src/data.js
@@ -9,48 +9,16 @@
 // export const pokemonCard = [pokemon];
 // console.log(pokemon)
 
-export default pokemon;
-
-// const filterDataPokemon = pokemon.filter(pokemon => pokemon.type === "poison" && pokemon.generation === "i");
+export function printCard ()
 
 
-// console.log(filterDataPokemon);
+let pokemonCard = [""]; 
+ console.log (pokemonCard)
 
-
-// sortData(data, sortBy, sortOrder)
-// pokemon.sort();
-
-// computeStats(data)
-
-
-// fetch('https://pokeapi.co/api/v2/type/3')
-//   .then(response => response.json())
-//   .then(data => {
-//     const pokemonOfType = data.pokemon.map(p => p.pokemon.name);
-//     const filteredPokemon = pokemonOfType.filter(name => name.startsWith('b'));
-
-//     console.log(filteredPokemon);
-//   })
-//   .catch(error => console.error(error));
-
-
-    let pokemonData = [];
-
-   
-      pokemonList.forEach(pokemon => {
-        const pokemonElement = document.createElement("div");
-        pokemonElement.innerHTML = `
-          <p>Name: ${pokemon.name}</p>
-          <p><a href="${pokemon.url}" target="_blank">View details</a></p>
-        `;
-        pokemonListElement.appendChild(pokemonElement);
-      });
-    
-
-    const pokemonSearchInput = document.getElementById("pokemon-search");
-    pokemonSearchInput.addEventListener("input", event => {
-      const searchQuery = event.target.value.toLowerCase();
-      const filteredPokemon = pokemonData.filter(pokemon => pokemon.name.includes(searchQuery));
-      displayPokemonList(filteredPokemon);
-    });
-    console.log(pokemonData)
+export function pokemonFilter (pokemon) {
+  if (pokemon.type === "fire") {
+    return true;
+  } else {
+    return false;
+  }
+};

03b7be40a839db7f8dc596d0770c18e7205437a1
Odett Dánae✨ <odettdpo@gmail.com>
2023-05-17 22:18:28 -0400

    Updates

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
 import data from "./data/pokemon/pokemon.js";
+import { pokemonFilter, pokemonSort } from "./data.js";
+let filterText = "";
 
-const printCard = (urlPokemon, namePokemon, numberPokemon, typePokemon) => {
+// Retorna una card con los datos de un pokemon
+const printCard = (
+  urlPokemon,
+  namePokemon,
+  numberPokemon,
+  typePokemon,
+  height,
+  weight
+) => {
   return `<div class="card">
     <img
       src="${urlPokemon}"
@@ -11,16 +21,47 @@ const printCard = (urlPokemon, namePokemon, numberPokemon, typePokemon) => {
       <p class="pokemon-name">${namePokemon}</p>
       <div class="pokemon-type">
         <p class="fire">${typePokemon}</p>
+        <div class="pokemon-size">
+        <p class="height"> H: ${height}</p>
+        <p class="weight"> W: ${weight}</p>
+        </div>
       </div>
     </div>
   </div>`;
 };
 
-data.pokemon.forEach((pokemon) => {
-  document.getElementById("cards").innerHTML += printCard(
-    pokemon.img,
-    pokemon.name,
-    pokemon.num,
-    pokemon.type
-  );
+const renderPokemon = () => {
+  const result = pokemonFilter(data.pokemon, filterText).map((pokemon) => {
+    return printCard(
+      pokemon.img,
+      pokemon.name,
+      pokemon.num,
+      pokemon.type,
+      pokemon.size.height,
+      pokemon.size.weight
+    );
+  });
+  document.getElementById("cards").innerHTML = result.join("");
+};
+
+// FILTER
+
+const filterForm = document.getElementById("formSearch");
+filterForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  filterText = document.getElementById("searchInput").value;
+
+  renderPokemon();
+});
+
+renderPokemon();
+
+// SORT
+
+const sortForm = document.querySelector(".formSort");
+sortForm.addEventListener("change", (event) => {
+  const sortValue = event.target.value;
+  // Llamamos a la función pokemonSort y actualizamos el orden de los pokemon
+  pokemonSort(data.pokemon, sortValue);
+  renderPokemon();
 });
